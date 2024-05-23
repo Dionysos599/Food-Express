c24846019568a0e7e16ef898d7710664f8446c91
Hinyeung Lam <hlam3@cs.washington.edu>
2024-05-23 16:08:33 -0700

    Remove Server folder for server-specific branch

--- Server/src/main/java/com/laioffer/onlineorder/repository/RestaurantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.laioffer.onlineorder.repository;
-
-import com.laioffer.onlineorder.entity.RestaurantEntity;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface RestaurantRepository extends ListCrudRepository<RestaurantEntity, Long> {
-}
